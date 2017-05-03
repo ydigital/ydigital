acc3d7c913fde35790180af7c16fa47f66d90033
Ahmad Arif Maulana <maulanaahmdarif@gmail.com>
2017-05-03 16:40:31 +0700

    Add team page

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get('/memahami-digital-marketing-dengan-mudah', function(req, res) {
 	res.sendFile(__dirname + '/public/news-detail.html');
 })
 
+app.get('/team', function(req, res){
+	res.sendFile(__dirname + '/public/team.html');
+})
+
 app.use((req, res, next) => {
 	res.status(404);
 	res.sendFile(__dirname + '/public/404.html');
